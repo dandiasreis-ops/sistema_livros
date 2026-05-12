c24114744c3259a17a100d0824f157978747f133
dandiasreis-ops <dandiasreis@gmail.com>
2026-05-12 17:44:30 -0300

    Adiciona suporte a CORS

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
@@ -15,6 +16,8 @@ jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
+    
+    CORS(app)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
